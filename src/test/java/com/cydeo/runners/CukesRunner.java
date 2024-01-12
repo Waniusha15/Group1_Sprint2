1f2b726b7a80b6d19574dd40246de8049c30d18e
waniusha <ion.nicolaescu15@icloud.com>
2024-01-12 16:36:30 +0000

    refactor Login.feature for jira link

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-//                "json:target/cucumber.json"
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
