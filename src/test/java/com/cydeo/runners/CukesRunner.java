aeb50d859d51026e6cae3c8445add98fb6163039
Roman <rbaidachenko@gmail.com>
2024-01-12 15:38:32 +0000

    refactor

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "",
-        publish = true
+        tags = "@Roman",
+        publish = false
       
 
 )
